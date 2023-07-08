d193a9fe58e780b47b9a8a3783820e267573326a
jonasty <jonas.berx@ut.ee>
2023-07-08 18:43:12 +0300

    Deploy v1 push

--- frontend/authConfig.ts
+++ frontend/authConfig.ts
@@ -1,10 +1,10 @@
 export const authConfig = {
   authority: 'http://zitadel.cloud.ut.ee', //Replace with your issuer URL
   client_id: '221885342960123907@pix', //Replace with your client id
-  redirect_uri: 'http://localhost:5173/api/auth/callback/zitadel',
+  redirect_uri: 'http://pix.cloud.ut.ee/api/auth/callback/zitadel',
   response_type: 'code',
   scope: 'openid profile email',
-  post_logout_redirect_uri: 'http://localhost:5173/',
+  post_logout_redirect_uri: 'http://pix.cloud.ut.ee/',
   userinfo_endpoint: 'http://zitadel.cloud.ut.ee/oidc/v1/userinfo', //Replace with your user-info endpoint
   response_mode: "query" as const,
   code_challenge_method: 'S256',
