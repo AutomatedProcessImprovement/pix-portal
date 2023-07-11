3579af4966a7be3d4dea0db3c49c1e0a644ad170
jonasty <jonas.berx@ut.ee>
2023-07-11 16:56:47 +0300

    api update form hardcode to env var

--- combined_client/src/prosimos_axios.ts
+++ combined_client/src/prosimos_axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_PROSIMOS_REACT_APP_BASE_URL,
 });
 
 export default instance;
