5696f431df7914eac22f3d1f8c7165005d4392ec
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-09-22 18:31:23 +0300

    Add tracing, #52

--- services/users/users/app.py
+++ services/users/users/app.py
@@ -82,3 +82,24 @@ async def on_startup():
             await migrate_to_latest()
     except Exception as e:
         print(e)
+
+
+def instrument_app(app: FastAPI, service_name: str):
+    from opentelemetry import trace
+    from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+    from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+    from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
+    from opentelemetry.sdk.resources import Resource, SERVICE_NAME
+    from opentelemetry.sdk.trace import TracerProvider
+    from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+    FastAPIInstrumentor.instrument_app(app)
+    HTTPXClientInstrumentor().instrument()
+    resource = Resource(attributes={SERVICE_NAME: service_name})
+    provider = TracerProvider(resource=resource)
+    processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://otel-collector:4317"))
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
+
+
+instrument_app(app, service_name="users")
