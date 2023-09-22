558b093ae20729eae3a06c1b5c3e7d9038b595b9
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-09-22 18:39:32 +0300

    Remove httpx tracing for users, #52

--- services/users/users/app.py
+++ services/users/users/app.py
@@ -88,13 +88,11 @@ def instrument_app(app: FastAPI, service_name: str):
     from opentelemetry import trace
     from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
     from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-    from opentelemetry.instrumentation.httpx import HTTPXClientInstrumentor
     from opentelemetry.sdk.resources import Resource, SERVICE_NAME
     from opentelemetry.sdk.trace import TracerProvider
     from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
     FastAPIInstrumentor.instrument_app(app)
-    HTTPXClientInstrumentor().instrument()
     resource = Resource(attributes={SERVICE_NAME: service_name})
     provider = TracerProvider(resource=resource)
     processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://otel-collector:4317"))
