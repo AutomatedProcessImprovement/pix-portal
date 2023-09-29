6001d40b4bbe4db4bb83a3dfe3d6259f4dddb009
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-09-29 17:43:06 +0300

    Add Kafka and Kafka producer for processing-requests service, #42

--- backend/services/processing-requests/processing_requests/settings.py
+++ backend/services/processing-requests/processing_requests/settings.py
@@ -10,6 +10,11 @@ class Settings(BaseSettings):
     asset_service_url: HttpUrl
     user_service_url: HttpUrl
     project_service_url: HttpUrl
+    kafka_bootstrap_servers: str
+    kafka_topic_simulation_prosimos: str
+    kafka_topic_process_model_optimization_simod: str
+    kafka_topic_process_model_optimization_optimos: str
+    kafka_topic_waiting_time_analysis_kronos: str
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env", extra="allow"
