afa5cabfb4f8f9d6a184fef8a191dc651f084347
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-10-16 19:53:19 +0300

    Update self_authenticating_client.py

--- backend/lib/pix_portal_lib/service_clients/self_authenticating_client.py
+++ backend/lib/pix_portal_lib/service_clients/self_authenticating_client.py
@@ -12,14 +12,14 @@ class SelfAuthenticatingClient:
     """
 
     def __init__(self):
-        self._auth_service = AuthServiceClient()
+        self._auth_service_client = AuthServiceClient()
         self._token = None
 
     @property
     async def token(self) -> str:
         if self._token is None:
             try:
-                self._token = await self._auth_service.get_system_jwt_token()
+                self._token = await self._auth_service_client.get_system_jwt_token()
             except Exception as e:
                 logger.error(f"Error getting system JWT token: {e}")
                 raise e
