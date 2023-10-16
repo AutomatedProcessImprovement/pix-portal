409820e40d710d2d69e6686f8586920d18da5911
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-10-16 19:54:01 +0300

    Make lib.service_clients.user a self-authenticating client

--- backend/lib/pix_portal_lib/service_clients/user.py
+++ backend/lib/pix_portal_lib/service_clients/user.py
@@ -1,27 +1,31 @@
+from typing import Optional
 from urllib.parse import urljoin
 from uuid import UUID
 
 import httpx
 from pix_portal_lib.utils import get_env
 
-from .utils import get_env
+from .self_authenticating_client import SelfAuthenticatingClient
 
 user_service_url = get_env("USER_SERVICE_URL")
 
 
-class UserServiceClient:
+class UserServiceClient(SelfAuthenticatingClient):
     def __init__(self):
+        super().__init__()
         self._client = httpx.AsyncClient()
         self._base_url = user_service_url
 
-    async def does_user_exist(self, user_id: UUID, token: str) -> bool:
+    async def does_user_exist(self, user_id: UUID, token: Optional[str] = None) -> bool:
         url = urljoin(self._base_url, str(user_id))
-        response = await self._client.get(url, headers={"Authorization": f"Bearer {token}"})
+        headers = await self.request_headers(token)
+        response = await self._client.get(url, headers=headers)
         return response.status_code == 200
 
-    async def get_user(self, user_id: UUID, token: str) -> dict:
+    async def get_user(self, user_id: UUID, token: Optional[str] = None) -> dict:
         url = urljoin(self._base_url, str(user_id))
-        response = await self._client.get(url, headers={"Authorization": f"Bearer {token}"})
+        headers = await self.request_headers(token)
+        response = await self._client.get(url, headers=headers)
         return response.json()
 
     async def get_users_by_ids(self, users_ids: list[UUID], token: str) -> list[dict]:
