06c394fe498d4090c60eba0630f9b6807a56712c
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-11-08 16:30:50 +0200

    Update frontend to use new assets and files #67

--- frontend/app/services/assets.server.ts
+++ frontend/app/services/assets.server.ts
@@ -1,4 +1,5 @@
 import { assetsURL, http } from "~/services/shared";
+import { AssetTypeBackend } from "../shared/AssetTypeBackend";
 
 export type Asset = {
   id: string;
@@ -8,26 +9,14 @@ export type Asset = {
   name: string;
   description?: string;
   type: string;
-  file_id: string;
   project_id: string;
+  files_ids: string[];
+  users_ids: string[];
   processing_requests_ids?: string[];
 };
 
-export enum AssetTypeBackend {
-  EVENT_LOG_CSV = "event_log_csv",
-  EVENT_LOG_CSV_GZ = "event_log_csv_gz",
-  EVENT_LOG_COLUMN_MAPPING_JSON = "event_log_column_mapping_json",
-  PROCESS_MODEL_BPMN = "process_model_bpmn",
-  CONFIGURATION_SIMOD_YAML = "configuration_simod_yaml",
-  SIMULATION_MODEL_PROSIMOS_JSON = "simulation_model_prosimos_json",
-  CONFIGURATION_PROSIMOS_YAML = "configuration_prosimos_yaml",
-  CONSTRAINTS_MODEL_OPTIMOS_JSON = "constraints_model_optimos_json",
-  WAITING_TIME_ANALYSIS_REPORT_KRONOS_JSON = "waiting_time_analysis_report_kronos_json",
-  WAITING_TIME_ANALYSIS_REPORT_KRONOS_CSV = "waiting_time_analysis_report_kronos_csv",
-}
-
 export async function createAsset(
-  fileId: string,
+  filesIds: string[],
   name: string,
   type: AssetTypeBackend,
   projectId: string,
@@ -37,8 +26,8 @@ export async function createAsset(
   const payload = {
     name: name,
     type: type,
-    file_id: fileId,
     project_id: projectId,
+    files_ids: filesIds,
   };
   const response = await http.post(url, payload, {
     headers: {
---
+++ frontend/app/shared/file_upload.server.ts
@@ -0,0 +1,174 @@
+import { EventLogColumnMapping } from "~/components/upload/column_mapping";
+import { createAsset, deleteAsset } from "~/services/assets.server";
+import { FileType, File as File_, deleteFile, uploadFile } from "~/services/files.server";
+import { AssetTypeBackend } from "~/shared/AssetTypeBackend";
+
+export async function createAssetsFromForm(formData: FormData, projectId: string, token: string) {
+  const assetType = formData.get("assetType") as AssetTypeBackend;
+
+  switch (assetType) {
+    case AssetTypeBackend.EVENT_LOG:
+      return await createEventLogFromForm(formData, projectId, token);
+    case AssetTypeBackend.PROCESS_MODEL:
+      return await createProcessModelFromForm(formData, projectId, token);
+    case AssetTypeBackend.SIMULATION_MODEL:
+      return await createSimulationModelFromForm(formData, projectId, token);
+    default:
+      throw new Error(`Unknown asset type ${assetType}`);
+  }
+}
+
+async function createEventLogFromForm(formData: FormData, projectId: string, token: string) {
+  // event log has 2 files, event log (CSV or CSV.GZ) and column mapping (JSON)
+  const eventLog = formData.get("eventLogFile") as File;
+  const eventLogColumnMapping = formData.get("eventLogColumnMapping") as string;
+  if (eventLog && eventLog.size > 0 && eventLogColumnMapping) {
+    await uploadAndCreateEventLog(eventLog as File, eventLogColumnMapping as string, projectId, token);
+  } else {
+    throw new Error("Event log file or column mapping missing");
+  }
+}
+
+async function createProcessModelFromForm(formData: FormData, projectId: string, token: string) {
+  const processModel = formData.get("processModelFile") as File;
+  if (processModel && processModel.size > 0) {
+    await uploadAndCreateProcessModel(processModel as File, projectId, token);
+  } else {
+    throw new Error("Process model file missing");
+  }
+}
+
+async function createSimulationModelFromForm(formData: FormData, projectId: string, token: string) {
+  // simulation model has 2 files, simulation model (JSON) and process model (BPMN)
+  const simulationModel = formData.get("simulationModelFile") as File;
+  const processModel = formData.get("processModelFile") as File;
+  if (simulationModel && simulationModel.size > 0 && processModel && processModel.size > 0) {
+    await uploadAndCreateSimulationModel(simulationModel as File, processModel as File, projectId, token);
+  } else {
+    throw new Error("Simulation model or process model file missing");
+  }
+}
+
+async function uploadAndCreateEventLog(
+  eventLog: File,
+  eventLogColumnMapping: string,
+  projectID: string,
+  token: string
+) {
+  const eventLogFileType = inferEventLogFileType(eventLog);
+  const columnMappingFile = makeColumnMappingFileFromForm(eventLogColumnMapping);
+  const files: FilePayload[] = [
+    filePayloadFromFile(eventLog, eventLogFileType),
+    filePayloadFromFile(columnMappingFile, FileType.EVENT_LOG_COLUMN_MAPPING_JSON),
+  ];
+
+  const uploadedFiles = await uploadFiles(files, token);
+
+  return await createAssetFromUploadedFiles(
+    {
+      name: eventLog.name,
+      files: uploadedFiles,
+      type: AssetTypeBackend.EVENT_LOG,
+      projectID: projectID,
+    },
+    token
+  );
+}
+
+async function uploadAndCreateProcessModel(processModel: File, projectID: string, token: string) {
+  const uploadedFiles = await uploadFiles([filePayloadFromFile(processModel, FileType.PROCESS_MODEL_BPMN)], token);
+  return await createAssetFromUploadedFiles(
+    {
+      name: processModel.name,
+      files: uploadedFiles,
+      type: AssetTypeBackend.PROCESS_MODEL,
+      projectID: projectID,
+    },
+    token
+  );
+}
+
+async function uploadAndCreateSimulationModel(
+  simulationModel: File,
+  processModel: File,
+  projectID: string,
+  token: string
+) {
+  const uploadedFiles = await uploadFiles(
+    [
+      filePayloadFromFile(simulationModel, FileType.SIMULATION_MODEL_PROSIMOS_JSON),
+      filePayloadFromFile(processModel, FileType.PROCESS_MODEL_BPMN),
+    ],
+    token
+  );
+  const name = processModel.name.split(".").slice(0, -1).join("."); // name is processModel.name without the .BPMN extension
+  return await createAssetFromUploadedFiles(
+    {
+      name: name,
+      files: uploadedFiles,
+      type: AssetTypeBackend.SIMULATION_MODEL,
+      projectID: projectID,
+    },
+    token
+  );
+}
+
+function inferEventLogFileType(file: File) {
+  const extension = file.name.split(".").pop();
+  const fileType = extension === "gz" ? FileType.EVENT_LOG_CSV_GZ : FileType.EVENT_LOG_CSV;
+  return fileType;
+}
+
+function filePayloadFromFile(file: File, fileType: FileType) {
+  return { name: file.name, file, type: fileType };
+}
+
+function makeColumnMappingFileFromForm(mapping: string) {
+  const columnMapping = EventLogColumnMapping.fromString(mapping);
+  const columnMappingFile = new File([columnMapping.toString()], "column_mapping.json");
+  return columnMappingFile;
+}
+
+type FilePayload = {
+  name: string;
+  file: File;
+  type: FileType;
+};
+
+async function uploadFiles(files: FilePayload[], token: string) {
+  let uploadedFiles;
+  try {
+    uploadedFiles = await Promise.all(
+      files.map(async (filePayload) => {
+        return await uploadFile(filePayload.file, filePayload.name, filePayload.type, token);
+      })
+    );
+  } catch (e) {
+    if (uploadedFiles) await Promise.all(uploadedFiles.map((file) => deleteFile(file.id, token)));
+    throw e;
+  }
+  return uploadedFiles;
+}
+type AssetPayload = {
+  name: string;
+  files: File_[];
+  type: AssetTypeBackend;
+  projectID: string;
+};
+
+async function createAssetFromUploadedFiles(payload: AssetPayload, token: string) {
+  let createdAsset;
+  try {
+    const filesIDs = payload.files.map((file) => file.id);
+    createdAsset = await createAsset(filesIDs, payload.name, payload.type, payload.projectID, token);
+  } catch (e) {
+    if (createdAsset) {
+      await Promise.all([
+        deleteAsset(createdAsset.id, token),
+        ...payload.files.map((file) => deleteFile(file.id, token)),
+      ]);
+    }
+    throw e;
+  }
+  return createdAsset;
+}
