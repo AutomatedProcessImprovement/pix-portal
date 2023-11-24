f3c993f8b7e94b7896d1fbf5260fa22f0088e50a
Ihar Suvorau <ihar.suvorau@gmail.com>
2023-11-24 13:30:38 +0200

    Fix resource calendars parsing, #62

--- frontend/app/components/processing/prosimos/simulation_parameters.ts
+++ frontend/app/components/processing/prosimos/simulation_parameters.ts
@@ -1,23 +1,45 @@
 import { prosimosConfigurationSchema } from "./schema";
 
+type CalendarPeriod = {
+  from: string;
+  to: string;
+  beginTime: string;
+  endTime: string;
+};
+
+type NamedEntity = {
+  id: string;
+  name: string;
+};
+
 export async function parseSimulationParameters(jsonBlob: Blob) {
   const jsonStr = await jsonBlob.text();
   const json = JSON.parse(jsonStr);
   let simulationParameters = await prosimosConfigurationSchema.validate(json);
 
-  simulationParameters.arrival_time_calendar = simulationParameters.arrival_time_calendar.map(
-    (calendar: { from: string; to: string; beginTime: string; endTime: string }) => {
+  simulationParameters.arrival_time_calendar =
+    simulationParameters.arrival_time_calendar.map(postProcessCalendarPeriod);
+
+  simulationParameters.resource_calendars = simulationParameters.resource_calendars.map(
+    (calendar: NamedEntity & { time_periods: CalendarPeriod[] }) => {
       return {
-        from: makeTitleCase(calendar.from),
-        to: makeTitleCase(calendar.to),
-        beginTime: removeFractionalTimePart(calendar.beginTime),
-        endTime: removeFractionalTimePart(calendar.endTime),
+        ...calendar,
+        time_periods: calendar.time_periods.map(postProcessCalendarPeriod),
       };
     }
   );
 
   console.log("simulationParameters", simulationParameters);
   return simulationParameters;
+}
+
+function postProcessCalendarPeriod(calendarPeriod: CalendarPeriod) {
+  return {
+    from: makeTitleCase(calendarPeriod.from),
+    to: makeTitleCase(calendarPeriod.to),
+    beginTime: removeFractionalTimePart(calendarPeriod.beginTime),
+    endTime: removeFractionalTimePart(calendarPeriod.endTime),
+  };
 }
 
 function makeTitleCase(str: string) {
