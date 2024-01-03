832ce573b1e99bce9ba126ad312a26768b3e67ba
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-01-03 13:37:44 +0200

    Add link to Kronos in a processing request cards, #63

--- frontend/app/routes/projects.$projectId.$processingType/components/ProcessingRequestCard.tsx
+++ frontend/app/routes/projects.$projectId.$processingType/components/ProcessingRequestCard.tsx
@@ -1,5 +1,6 @@
+import { Link } from "@remix-run/react";
 import { Suspense } from "react";
-import type { ProcessingRequest } from "~/services/processing_requests";
+import { ProcessingRequestType, type ProcessingRequest } from "~/services/processing_requests";
 import { parseDate } from "~/shared/utils";
 
 export function ProcessingRequestCard({ request }: { request: ProcessingRequest }) {
@@ -17,11 +18,18 @@ export function ProcessingRequestCard({ request }: { request: ProcessingRequest 
   const duration = request.end_time ? formatDuration(getDuration(request.creation_time, request.end_time)) : "";
 
   return (
-    <div className="px-2 flex flex-col break-words">
+    <div className="p-2 flex flex-col break-words tracking-normal text-sm text-slate-900">
       <Suspense fallback={<div>Loading...</div>}>
-        {parseDate(request.creation_time)}
-        {request.end_time ? "–" + parseDate(request.end_time!) : ""}: {request.status}{" "}
-        {duration ? `in ${duration}` : ""}
+        <div>Started: {parseDate(request.creation_time)}</div>
+        <div>
+          Status: <span className="font-semibold">{request.status}</span>
+        </div>
+        {duration ? <div>Duration {duration}</div> : <></>}
+        {request.type === ProcessingRequestType.WAITING_TIME_ANALYSIS_KRONOS && (
+          <Link to={`/kronos/results/${request.id}`} target="_blank" className="shrink w-fit">
+            Switch to Kronos
+          </Link>
+        )}
       </Suspense>
     </div>
   );
