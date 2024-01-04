13289fa17a29ea0870eef9f96452e7948ae69722
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-01-04 16:32:46 +0200

    Fix NavBar image import

--- frontend/kronos-web-ui/src/app/components/NavBar.tsx
+++ frontend/kronos-web-ui/src/app/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import logotype from "../../../public/kronos-logo.png";
 
 export default function NavBar() {
   return (
     <nav className="bg-blue-500 h-16 flex p-4 shadow-md shadow-blue-200 mb-4">
       <span className="flex space-x-4 text-white items-center">
-        <Image src="/kronos-logo.png" alt="Kronos Logotype" height={40} width={50} priority={false} />
+        <Image src={logotype} alt="Kronos Logotype" height={40} width={50} priority={false} />
         <span className="text-xl font-semibold tracking-wide">Kronos</span>
       </span>
     </nav>
