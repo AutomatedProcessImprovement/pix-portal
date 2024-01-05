b67e203fedc8dc9563ec8e1d01e57ada7a47512b
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-01-05 17:02:21 +0200

    Add backend URL env to next.config.js

--- frontend/kronos-web-ui/next.config.js
+++ frontend/kronos-web-ui/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: "standalone",
   basePath: "/kronos",
+  env: {
+    NEXT_PUBLIC_KRONOS_HTTP_URL: process.env.NEXT_PUBLIC_KRONOS_HTTP_URL,
+  },
 };
 
 module.exports = nextConfig;
