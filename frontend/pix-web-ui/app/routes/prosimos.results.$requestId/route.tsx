7b8a807d20045ead36d1c805089de59677bae638
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-01-22 13:52:24 +0200

    Redirect to login page on 401 error for Prosimos results UI

--- frontend/pix-web-ui/app/routes/prosimos.results.$requestId/route.tsx
+++ frontend/pix-web-ui/app/routes/prosimos.results.$requestId/route.tsx
@@ -1,5 +1,6 @@
-import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { json, redirect, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
 import { isRouteErrorResponse, useLoaderData, useRouteError } from "@remix-run/react";
+import type { AxiosError } from "axios";
 import { getAsset } from "~/services/assets.server";
 import { FileType } from "~/services/files";
 import { getProcessingRequest } from "~/services/processing_requests.server";
@@ -30,10 +31,23 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   const user = await requireLoggedInUser(request);
   const requestId = params.requestId as string;
 
-  const processingRequest = await getProcessingRequest(requestId, user.token!);
-  if (processingRequest.output_assets_ids.length === 0) throw new Response("No output assets found", { status: 404 });
+  let processingRequest, outputAsset;
+  try {
+    processingRequest = await getProcessingRequest(requestId, user.token!);
+    if (processingRequest?.output_assets_ids.length === 0) {
+      throw new Response("No output assets found", { status: 404 });
+    }
+    outputAsset = await getAsset(processingRequest.output_assets_ids[0], false, user.token!);
+  } catch (e: any) {
+    const err = e as AxiosError;
+    if (err.response?.status === 401) {
+      throw redirect("/login");
+    }
+    const status = err.response?.status || 500;
+    const message = (err.response?.data as string) || err.message || "Error";
+    throw new Response(message, { status: status });
+  }
 
-  const outputAsset = await getAsset(processingRequest.output_assets_ids[0], false, user.token!);
   const prosimosReportCsv = outputAsset.files?.find((file) => file.type === FileType.STATISTICS_PROSIMOS_CSV);
   if (!prosimosReportCsv) throw new Response("No Prosimos report found", { status: 404, statusText: "Not Found" });
   const report = await parseCsvFile(prosimosReportCsv.id, user.token!);
@@ -45,12 +59,12 @@ export default function ProsimosStatisticsPage() {
 
   if (!report) throw new Error("No report found");
   return (
-    <div className="flex flex-col p-8 items-center justify-center">
+    <main className="flex flex-col p-8 items-center justify-center overflow-auto">
       <h1 className="text-3xl font-semibold">Simulation Statistics</h1>
       {report && <ScenarioStatistics data={report[2].data as ScenarioStatisticsItem[]} />}
       {report && <ResourceUtilization data={report[0].data as ResourceUtilizationItem[]} />}
       {report && <IndividualTaskStatistics data={report[1].data as IndividualTaskStatisticsItem[]} />}
-    </div>
+    </main>
   );
 }
 
@@ -61,7 +75,7 @@ export function ErrorBoundary() {
       <div className="h-full flex flex-col justify-center items-center">
         <div className="m-8 text-center">
           <p className="text-3xl">
-            <span className="text-red-600">{error.statusText}</span>
+            <span className="text-red-600">{error.statusText || "Error"}</span>
           </p>
           <p className="text-base mt-2">{error.data}</p>
         </div>
