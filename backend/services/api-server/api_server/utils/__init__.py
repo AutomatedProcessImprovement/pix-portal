979d68c83e4d4c380c2ee6649c1c71e8e1700d51
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-02-07 12:47:44 +0200

    Remove service_clients from __init__

--- backend/services/api-server/api_server/utils/__init__.py
+++ backend/services/api-server/api_server/utils/__init__.py
@@ -2,14 +2,12 @@ from . import exceptions
 from . import middleware
 from . import open_telemetry_utils
 from . import persistence
-from . import service_clients
 from . import utils
 
 __all__ = [
     "open_telemetry_utils",
     "exceptions",
     "middleware",
-    "service_clients",
     "utils",
     "persistence",
 ]
