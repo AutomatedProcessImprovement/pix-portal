4adf765d4209930ecc6dd5cc67d45c397ec7edd8
Jannis Rosenbaum <j.ro@dock51.de>
2024-06-17 13:18:00 +0300

    increase jwt time to one week

--- backend/services/api-server/api_server/users/users.py
+++ backend/services/api-server/api_server/users/users.py
@@ -123,7 +123,7 @@ bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+    return JWTStrategy(secret=SECRET, lifetime_seconds=60 * 60 * 24 * 7)
 
 
 auth_backend = AuthenticationBackend(
